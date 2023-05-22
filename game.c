ca1b6b9c67b65f45072649f1fc84768ab22d8bd1
Conor McGovern <conor@cmgn.io>
2023-05-22 16:17:51 +0100

    Add some overlay text

--- game.c
+++ game.c
@@ -5,6 +5,8 @@
 
 #include <SDL2/SDL.h>
 
+#include "font8x8_basic.h"
+
 #define WINDOW_WIDTH 1024
 #define WINDOW_HEIGHT 768
 
@@ -13,6 +15,8 @@
 
 #define CELL_WIDTH ((WINDOW_WIDTH)/(GRID_WIDTH))
 #define CELL_HEIGHT ((WINDOW_HEIGHT)/(GRID_HEIGHT))
+
+#define OVERLAY_TEXT_SIZE 3
 
 enum tile_type {
 	TILE_GRASS,
@@ -25,6 +29,7 @@ struct tile {
 	enum tile_type type;
 };
 
+static const char *overlay_text = "CITYSIM";
 static SDL_Window *window = 0;
 static SDL_Renderer *renderer = 0;
 static struct tile grid[GRID_HEIGHT][GRID_WIDTH] = { 0 };
@@ -36,6 +41,41 @@ static const char *tile_texture_paths[TILE_TYPE_COUNT] = {
 		/* TILE_ROAD  */ "assets/road.bmp",
 };
 static SDL_Texture *tile_textures[TILE_TYPE_COUNT] = { 0 };
+static SDL_Texture *overlay_text_texture = 0;
+
+static unsigned int sdl_color_to_uint32(const SDL_Color *c)
+{
+	unsigned int ret = 0;
+	ret |= (unsigned int)(c->a) << 24;
+	ret |= (unsigned int)(c->r) << 16;
+	ret |= (unsigned int)(c->g) <<  8;
+	ret |= (unsigned int)(c->b) <<  0;
+	return ret;
+}
+
+static void write_text(SDL_Surface *surface, int x, int y, const char *text,
+		       int size)
+{
+	const SDL_Color foreground = { 255, 255, 255, 255 };
+	const SDL_Color background = {   0,   0,   0,   0 };
+	const unsigned int fgc = sdl_color_to_uint32(&foreground);
+	const unsigned int bgc = sdl_color_to_uint32(&background);
+	for (int k = 0; text[k]; k++) {
+		int c = text[k];
+		for (int j = 0; j < 8; j++) {
+			for (int i = 0; i < 8; i++) {
+				SDL_Rect rect = { x + (i + k * 8) * size,
+						  y + j * size, size, size };
+				int bit = font8x8_basic[c][j] & (1 << i);
+				if (bit) {
+					SDL_FillRect(surface, &rect, fgc);
+				} else {
+					SDL_FillRect(surface, &rect, bgc);
+				}
+			}
+		}
+	}
+}
 
 static int init_rendering()
 {
@@ -53,10 +93,30 @@ static int init_rendering()
 				SDL_GetError());
 			return -1;
 		}
-		SDL_FreeSurface(surface);
 		tile_textures[i] = texture;
 	}
+	SDL_Surface *overlay_text_surface = SDL_CreateRGBSurface(
+		0, strlen(overlay_text) * 8 * OVERLAY_TEXT_SIZE,
+		8 * OVERLAY_TEXT_SIZE, 32, 0, 0, 0, 0
+	);
+	if (!overlay_text_surface) {
+		SDL_Log("Failed to create overlay text surface: %s",
+			SDL_GetError());
+		return -1;
+	}
+	write_text(overlay_text_surface, 0, 0, overlay_text,
+		   OVERLAY_TEXT_SIZE);
+	overlay_text_texture = SDL_CreateTextureFromSurface(
+		renderer, overlay_text_surface);
+	SDL_FreeSurface(overlay_text_surface);
 	return 0;
+}
+
+static void render_overlay_text()
+{
+	SDL_Rect rect = { 10, 10, strlen(overlay_text) * OVERLAY_TEXT_SIZE * 8,
+			  OVERLAY_TEXT_SIZE * 8 };
+	SDL_RenderCopy(renderer, overlay_text_texture, 0, &rect);
 }
 
 static void render()
@@ -69,6 +129,7 @@ static void render()
 			SDL_RenderCopy(renderer, texture, 0, &rect);
 		}
 	}
+	render_overlay_text();
 }
 
 static float random_float()
